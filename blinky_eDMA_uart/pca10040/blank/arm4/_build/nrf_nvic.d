e3d7b7437631493bdf3f215b9e95afc5660984ea
Roman Gaisin <roma-cezar@rambler.ru>
2018-05-21 11:27:17 +0500

    added new project
    eDMA uarte0 nrf52

---
+++ blinky_eDMA_uart/pca10040/blank/arm4/_build/nrf_nvic.d
@@ -0,0 +1,16 @@
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\drivers_nrf\nrf_soc_nosd\nrf_nvic.c
+.\_build\nrf_nvic.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\drivers_nrf\nrf_soc_nosd\nrf_soc.h
+.\_build\nrf_nvic.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\device\nrf.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\device\nrf52.h
+.\_build\nrf_nvic.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm4.h
+.\_build\nrf_nvic.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
+.\_build\nrf_nvic.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
+.\_build\nrf_nvic.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm4_simd.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\toolchain\system_nrf52.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\device\nrf52_bitfields.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\device\nrf51_to_nrf52.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\device\nrf52_name_change.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\device\compiler_abstraction.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\drivers_nrf\nrf_soc_nosd\nrf_error.h
